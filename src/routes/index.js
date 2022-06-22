616e1ad6694559fdd7ec121889b0b050f6d498b7
Stephen <hellostephenwm@gmail.com>
2022-06-22 01:14:41 -0400

    Wrapped get request in try-catch

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,24 @@
+const config = require('../config')
 const router = require('express').Router()
+
+config.colors.enable()
 
 router.get('/', (req, res) => {
     // TODO GET index
-    res.status(200).json({
-        success: true,
-        message: 'Index route'
-    })
+    try {
+        res.status(200).json({
+            success: true,
+            message: 'Index route'
+        })
+    }
+    catch(error) {
+        res.status(500).json({
+            success: 'false',
+            message: error.message
+        })
+
+        console.log(error.message.red)
+    }
 })
 
 module.exports = router
