deab138be07bb8fe66fdd4499e185c78661d0053
Stephen <hellostephenwm@gmail.com>
2022-06-22 01:44:11 -0400

    Configured EJS as frontend engine

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const http = require('http')
 const config = require('./config')
+const path = require('path')
+const ejsLayouts = require('express-ejs-layouts')
 const app = express()
 const server = http.createServer(app)
 
@@ -9,9 +11,13 @@ const indexRouter = require('./routes/index')
 
 config.colors.enable()
 
+app.set('view engine', 'ejs')
+app.set('views', path.resolve(__dirname, '../client/views'))
+app.set('layout', 'layouts/layout')
 app.set('case sensitive routing', true)
 app.set('json spaces', 2)
 
+app.use(ejsLayouts)
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(config.cors())
